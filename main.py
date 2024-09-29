c9b64508fd459f21c4a0d519b3421ffa4a32e336
meganwijdoogen <mwijdoogen@loyola.edu>
2024-09-28 23:56:00 -0400

    Commit algorithm new

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Course: Cs151, Professor Zee
 # Due Date: 9/25/2024
 # Lab Assignment: 2
+
 # Problem statement: Create a program that will determine the expected population in the future /
 # (a certain number of years away) for a country based on a current population amount if you are /
 # given (1) how often someone is born (in seconds), (2) how often someone dies (in seconds), and /
@@ -10,6 +11,8 @@
 # Data In: Code needs input on how many seconds between birth, how many seconds between death, how /
 # many seconds between immigrations, what the current population is, how many years in the future you want to see
 # Data Out: The population change and if the population increased, decreased or is the same
+
+# The purpose of program is to give the population change when the user enters 5 values.
 
 # User should be prompted to enter five inputs: seconds before birth, seconds between death,/
 # seconds between immigration current population size, and number of years in the future.
@@ -28,11 +31,11 @@ num_years = float(input())
 
 # Code should calculate population change and print it.
 pop_change = ((sec_year/secs_births + sec_year/secs_immigrations - sec_year/secs_deaths) * num_years)
-print("The population change is", pop_change, 'people')
+print("The population change is", int(pop_change), 'people')
 
 # Code should calculate future population and print it.
 future_pop = (population + pop_change)
-print("The future population is", future_pop, 'people')
+print("The future population is", int(future_pop), 'people')
 
 # Code should use "if" statement to compare results of/
 # population change and future calculation to output various statements
